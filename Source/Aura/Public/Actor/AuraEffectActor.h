893b7267d4c701eda69ce36d808442d9544daaa8
PETER HAN <yesungpapa@gmail.com>
2023-11-23 22:35:38 -0800

    38-2. Effect Actor Improved

--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -29,9 +29,9 @@ protected:
 	virtual void BeginPlay() override;
 
 	UFUNCTION(BlueprintCallable)
-	void ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass);
+	void ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass);
 
-	UPROPERTY(EditAnywhere, Category = "Appplied Effect")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Appplied Effect")
 	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
 
 public:	
